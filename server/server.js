e187b8b020c259ccb4a82c53e81b448b20205e4b
yassertarek <yasser.tarek@uflare.io>
2020-02-16 22:00:06 +0200

    enable_cors

--- server/server.js
+++ server/server.js
@@ -31,7 +31,20 @@ var full_address;
 
 var app = express();
 app.use(bodyParser.json());
-app.use(express.static('uploads'))
+app.use(express.static('uploads'));
+app.use(function (req, res, next) {
+    
+    // Website you wish to allow to connect
+    res.setHeader('Access-Control-Allow-Origin', '*');
+  
+    // Request methods you wish to allow
+    res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+  
+    // Request headers you wish to allow
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+  
+    next();
+  });
 
 
 // deafult 
