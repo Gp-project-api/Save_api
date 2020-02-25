4f770c9ef3c0f9e5fca7353043e4cff47ac5f053
yassertarek <yasser.tarek@uflare.io>
2020-02-25 18:18:39 +0200

    X-AUTH

--- server/server.js
+++ server/server.js
@@ -41,7 +41,7 @@ app.use(function (req, res, next) {
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
   
     // Request headers you wish to allow
-    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization');
+    res.setHeader('Access-Control-Allow-Headers', 'Access-Control-Allow-Headers, Origin,Accept, X-Requested-With, Content-Type, Access-Control-Request-Method, Access-Control-Request-Headers,X-Access-Token,XKey,Authorization','X-AUTH');
    
     // Request headers credintials you wish to allow
      res.setHeader('Access-Control-Allow-Credentials', true)
